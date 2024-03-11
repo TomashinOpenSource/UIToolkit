1e85eb5681507d61ff85e79ecec758fbb63ce245
tomashined <tomashined@gmail.com>
2024-03-11 14:20:32 +0200

    Slider

--- Assets/UI/Controls/Slider/SliderScene.cs
+++ Assets/UI/Controls/Slider/SliderScene.cs
@@ -6,9 +6,21 @@ using UnityEngine.UIElements;
 
 public class SliderScene : MonoBehaviour
 {
+    private VisualElement box;
+    private Slider _slider;
+    
     private void OnEnable()
     {
         UIDocument ui = GetComponent<UIDocument>();
         VisualElement root = ui.rootVisualElement;
+        
+        box = root.Q<VisualElement>("Box");
+        _slider = root.Q<Slider>("MySlider");
+        
+        _slider.RegisterValueChangedCallback(OnSliderValueChanged);
+    }
+    private void OnSliderValueChanged(ChangeEvent<float> evt)
+    {
+        box.style.width = evt.newValue * 10;
     }
 }
