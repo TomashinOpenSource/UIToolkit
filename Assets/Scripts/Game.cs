aff514ed8378dc40563cb443ea910fc7862a095a
tomashined <tomashined@gmail.com>
2024-03-20 13:01:13 +0200

    147. Implementing The Game Logic

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -17,4 +17,48 @@ public class Game : MonoBehaviour
         currentQuestion = questions[questionIndex];
         currentHint = currentQuestion.GetHints()[hintIndex];
     }
+    
+    public bool IsAnswerCorrect(string answer)
+    {
+        return currentQuestion.answer == answer;
+    }
+
+    public void HandleCorrectAnswer()
+    {
+        
+    }
+    public void HandleWrongAnswer()
+    {
+        if (hintIndex < 2)
+        {
+            currentHint = currentQuestion.GetHints()[++hintIndex];
+        }
+        else
+        {
+            NextQuestion();
+        }
+    }
+    private void NextQuestion()
+    {
+        currentQuestion = questions[++questionIndex];
+        hintIndex = 0;
+        currentHint = currentQuestion.GetHints()[hintIndex];
+    }
+
+    public Question getCurrentQuestion()
+    {
+        return currentQuestion;
+    }
+    public int getCurrentQuestionNum()
+    {
+        return questionIndex + 1;
+    }
+    public string getCurrentHint()
+    {
+        return currentHint;
+    }
+    public int getCurrentHintNum()
+    {
+        return hintIndex + 1;
+    }
 }
