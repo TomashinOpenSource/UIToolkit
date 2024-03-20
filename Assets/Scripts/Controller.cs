af855e9a672b397c000409de3c623eb1bc6dc6db
tomashined <tomashined@gmail.com>
2024-03-20 13:07:30 +0200

    148. Connecting The Game Controller

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Game game;
+    //public UI ui;
+
+    public void Initialize()
+    {
+        game.InitializeGame();
+        
+    }
+    
+    public void HandleWrongAnswer()
+    {
+        game.HandleWrongAnswer();
+        UpdateUI();
+    }
+    
+    public void HandleCorrectAnswer()
+    {
+        game.HandleCorrectAnswer();
+        UpdateUI();
+    }
+    
+    public void UpdateUI()
+    {
+        
+    }
+}
